1a0ece34d6361ce59dfcf3512ae1edde6e86dd84
jazer <60688719+Muhmmed-jasir-pt@users.noreply.github.com>
2022-01-06 00:23:58 +0530

    Add files via upload
    To fetch data address.

---
+++ address(data).cpp
@@ -0,0 +1,17 @@
+//written by jazer__________________________________________________
+
+//Program to find address of variables/////
+
+#include <iostream>
+
+using namespace std;
+int  main()
+{
+int j=89;
+float k=3.15;
+int x=85;
+    cout<<"Address of x = "<<&x<<"\n";//'&' is used to access the address of a data
+    cout<<"Address of x = "<<&k<<"\n";
+    cout<<"Address of x = "<<&j<<"\n";
+return 0;
+}
