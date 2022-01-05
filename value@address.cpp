9e74056cdb2f5a46b234b0b8e403d0c0c5bbfa9e
jazer <60688719+Muhmmed-jasir-pt@users.noreply.github.com>
2022-01-06 00:44:49 +0530

    Add files via upload
    fetch data from a RAM location.

---
+++ value@address.cpp
@@ -0,0 +1,15 @@
+//written by jazer__________________________________________________
+
+//Program to fetch a data from a address/////
+
+#include <iostream>
+
+using namespace std;
+int  main()
+{
+int j=89;
+int *k=&j;//'*k' is a special variable called pointer variable to store addresses.
+
+    cout<<"Address of x = "<<k<<"\n";
+return 0;
+}
